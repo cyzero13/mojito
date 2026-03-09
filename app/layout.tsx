99467903648813b1cbcf0dc4044b652d9d7f1503
Deony Diras <deony.diras1998@gmai.com>
2026-03-10 02:28:27 +0800

    added navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="w-full overflow-x-hidde">
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@
 import { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-
+import Navigation from "./components/Navigation";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Page() {
 
   return (
-    <div >page</div>
+    <div className="flex justify-center items-center">
+      <Navigation />
+    </div>
   );
 }
