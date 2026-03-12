09354ee43b14314ece6ee98498e121886e053700
Deony Diras <deony.diras1998@gmai.com>
2026-03-12 23:04:11 +0800

    added basepath and prefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: "/mojito",
+    assetPrefix: "/mojito/",
     output: "export",
     images: {
       unoptimized: true
